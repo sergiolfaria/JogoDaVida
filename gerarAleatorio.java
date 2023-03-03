83e6b5856e286802ec78cfc0ab69b36ca43e8083
sergiolfaria <srgion@yahoo.com.br>
2023-03-03 15:34:23 -0300

    matrizRadomNaoFinalizada

---
+++ gerarAleatorio.java
@@ -0,0 +1,8 @@
+class gerarAleatorio{
+   public static void main (String args[]){
+      int min = 0;
+      int max = 1;
+      System.out.println(""+ min + " to " + max + ":");
+      int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+   }
+}
