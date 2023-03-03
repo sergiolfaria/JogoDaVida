90a6d754bbff1e1a7b86b913969a37753bf919e6
sergiolfaria <srgion@yahoo.com.br>
2023-03-03 18:02:54 -0300

    correçao metodo

--- gerarAleatorio.java
+++ gerarAleatorio.java
@@ -1,9 +1,9 @@
 class gerarAleatorio{
-   public static void Aleatorio (String args[]){
+   public static void Aleatorio (){
       int min = 0;
       int max = 1;
-      System.out.println(""+ min + " to " + max + ":");
       int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
-      System.out.println(random_int);
+      int valor = random_int;
+      
    }
-}
+} 
