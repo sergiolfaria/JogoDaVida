ce467001fcd56d5234e43f70316d9c09e4321f9d
sergiolfaria <srgion@yahoo.com.br>
2023-03-04 05:00:49 -0300

    repetição para imprimir a matriz

--- jogoVida.java
+++ jogoVida.java
@@ -1,3 +1,10 @@
 public class jogoVida{
-  
+   public int Tabuleiro(){
+    for (int[] linha : matriz) {
+            for (int coluna : linha) {
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+   
 }
